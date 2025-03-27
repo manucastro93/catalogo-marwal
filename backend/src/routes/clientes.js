13db04360fcab7fd5ac86a199715f598b2fc3de7
manucastro93 <j.manuelcastro@hotmail.com>
2025-03-27 12:37:05 -0300

    Delete backend directory

--- backend/src/routes/clientes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerCliente } = require('../controllers/cliente');
-const auth = require('../middleware/auth');
-
-// Ruta para registrar un cliente utilizando el enlace único del vendedor
-router.post('/register', auth(), registerCliente);
-
-module.exports = router;
